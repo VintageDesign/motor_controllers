8a2488448b1ed5161b96b09e48dcc438d67f386c
Riley Mathieu <riley.mathieu@ravenind.com>
2025-08-12 13:09:43 -0400

    Add basic Stepper motor control
    Its pretty rough and blocks the caller. I wanted to get more advanced
    with it I just ran out of time. Next free sprint...

--- include/stepper.h
+++ include/stepper.h
@@ -1,94 +1,78 @@
 #ifndef STEPPER_H
 #define STEPPER_H
 
-#include "esp_log.h"
+#include <esp_log.h>
 #include <driver/gpio.h>
 #include <driver/gptimer.h>
 
+#define NS_TO_T_TICKS(x) (x)
+#define TIMER_F 1000000ULL
+#define TICK_PER_S TIMER_F
 
-enum motor_status
-{
-    DISABLED,
-    IDLE,
-    ACC,
-    COAST,
-    DEC
-};
+enum motor_status { DISABLED, IDLE, ACC, COAST, DEC };
 
-enum dir
-{
-    CW,
-    CCW
-};
+enum dir { CW, CCW };
 
 typedef enum {
-    MICROSTEP_1 = 0x1,
-    MICROSTEP_2 = 0x2,
-    MICROSTEP_4 = 0x4,
-    MICROSTEP_8 = 0x8,
-    MICROSTEP_16 = 0x10,
-    MICROSTEP_32 = 0x20,
-    MICROSTEP_64 = 0x40,
-    MICROSTEP_128 = 0x80,
-    MICROSTEP_256 = 0x100,
+  MICROSTEP_1 = 0x1,
+  MICROSTEP_2 = 0x2,
+  MICROSTEP_4 = 0x4,
+  MICROSTEP_8 = 0x8,
+  MICROSTEP_16 = 0x10,
+  MICROSTEP_32 = 0x20,
+  MICROSTEP_64 = 0x40,
+  MICROSTEP_128 = 0x80,
+  MICROSTEP_256 = 0x100,
 } micro_stepping_t;
 
+class Stepper {
+public:
+  Stepper(uint8_t step_pin, uint8_t dir_pin, uint8_t en_pin,
+          micro_stepping_t microsteps, uint16_t steps_per_rotation);
+  ~Stepper();
 
-class Stepper
-{
-    public:
-    Stepper();
-    ~Stepper();
+  void write_rad(float relative_rad);
+  void write_absolute_rad(float absolute_rad);
 
-    void attach(uint8_t step_pin, uint8_t dir_pin, uint8_t en_pin, micro_stepping_t microsteps, uint16_t steps_per_rotation);
-    void detach();
+  void set_speed(float speed_rad_per_sec, uint16_t acceleration_ms,
+                 uint16_t decceleration_ms);
 
-    eps_err_t write_rad(int32_t relative_rad);
-    eps_err_t write_absolute_rad(uint32_t absolute_rad);
-    
-    void set_speed(uitn32_t speed_rad_per_sec, uint16_t acceleration_ms, uint16_t decelleration_ms);
+  void disable_motor();
+  void enable_motor();
 
-    private:
+  void set_dir(bool dir_state);
+private:
+  void set_en(bool en_state);
 
-    void set_en(bool en_state);
-    void set_dir(bool dir_state);
+  static bool xISRwrap(gptimer_t *timer, const gptimer_alarm_event_data_t *data,
+                       void *_this) {
+    return static_cast<Stepper *>(_this)->xISR(timer, data);
+  }
 
+  static void _disableMotor(void *_this) {
+    static_cast<Stepper *>(_this)->disable_motor();
+  }
 
-	static bool xISRwrap(gptimer_t* timer, const gptimer_alarm_event_data_t* data,void *_this)
-	{
-		return static_cast<Stepper *>(_this)->xISR(timer,data);
-	}
+  bool xISR(gptimer_t *timer, const gptimer_alarm_event_data_t *data);
 
-	static void _disableMotor(void *_this)
-	{
-		static_cast<Stepper *>(_this)->disableMotor();
-	}
+  uint8_t m_step_pin = 0;
+  uint8_t m_dir_pin = 0;
+  uint8_t m_en_pin = 0;
+  micro_stepping_t m_microsteps;
+  uint16_t m_steps_per_rotation;
+  uint32_t m_step_interval = 40000; // interval in ns/25
 
-	bool xISR(gptimer_t* timer, const gptimer_alarm_event_data_t* data);
+  bool m_dir = 0;
+  uint64_t m_current_pos_steps = 0;
+  uint64_t m_step_count = 0;
+  uint64_t m_steps_to_go = 0;
 
+  float m_speed_rad_per_sec = 100;
 
-	uint8_t m_step_pin = 0;
-	uint8_t m_dir_pin = 0;
-	uint8_t m_en_pin = 0;
-	micro_stepping_t m_microstep;
-	uint16_t m_steps_per_rotation;
-	uint32_t m_step_interval = 40000; // interval in ns/25
-
-	bool m_dir = 0;
-    uint64_t m_current_pos_steps = 0;
-    uint64_t m_step_count = 0;
-    uint64_t m_steps_to_go = 0;
-
-	uint16_t m_speed_rad_per_sec = 100;
-
-
-
-    
-    gptimer_handle_t m_timer_handle;
-    gptimer_alarm_config_t alarm_config = { .reload_count = 0};
-
-
-}
-
+  gptimer_handle_t m_timer_handle;
+  gptimer_alarm_config_t m_alarm_config = {
+      .reload_count = 0,
+  };
+};
 
 #endif
--- src/stepper.cpp
+++ src/stepper.cpp
@@ -1,83 +1,124 @@
 #include "stepper.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #include <math.h>
 
+#include "hal/gpio_ll.h"
 #include <esp_log.h>
 
-Stepper::Stepper(uint8_t step_pin, uint8_t dir_pin, uint8_t en_pin, micro_stepping_t microsteps, uint16_t steps_per_rotation):
-m_step_pin(step_pin),
-    m_dir_pin(dir_pin),
-    m_en_pin(en_pin),
-    m_microsteps(microsteps),
-    m_steps_per_rotation(steps_per_rotation)
-{
-    uint64_t gpio_mask = ( 1ULL << m_step_pin 
-                         | 1ULL << m_dir_pin
-                         | 1ULL << m_en_pin);
-    gpio_config_t gpio_conf = {
-        .pin_bit_mask = gpio_mask,
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
+static const char* TAG = "StepperDriver";
 
-    ESP_ERROR_CHECK(gpio_config(&gpio_config));
+Stepper::Stepper(uint8_t step_pin, uint8_t dir_pin, uint8_t en_pin,
+                 micro_stepping_t microsteps, uint16_t steps_per_rotation)
+    : m_step_pin(step_pin), m_dir_pin(dir_pin), m_en_pin(en_pin),
+      m_microsteps(microsteps), m_steps_per_rotation(steps_per_rotation) {
+  uint64_t gpio_mask =
+      (1ULL << m_step_pin | 1ULL << m_dir_pin | 1ULL << m_en_pin);
+  gpio_config_t gpio_conf = {
+      .pin_bit_mask = gpio_mask,
+      .mode = GPIO_MODE_OUTPUT,
+      .pull_up_en = GPIO_PULLUP_DISABLE,
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,
+      .intr_type = GPIO_INTR_DISABLE,
+  };
 
-    gptimer_config_t timer_conf =
-    {
-        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-        .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = TIMER_F,
-    };
+  ESP_ERROR_CHECK(gpio_config(&gpio_conf));
 
-    timer_conf.intr_shared = false;
+  gptimer_config_t timer_conf = {
+      .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+      .direction = GPTIMER_COUNT_UP,
+      .resolution_hz = TIMER_F,
+  };
 
-    ESP_ERROR_CHECK(gptimer_new_timer(&timer_conf, &m_timer_handle)
+  ESP_ERROR_CHECK(gptimer_new_timer(&timer_conf, &m_timer_handle));
 
-
-    gptimer_event_callbacks_t cb_group;
-    cb_group.on_alarm = xISRwrap;
-    alarm_cfg.flags.auto_reload_on_alarm = 1;
-    gptimer_register_event_callbacks(m_timer_handle, &cb_group, this);
-
+  gptimer_event_callbacks_t cb_group;
+  cb_group.on_alarm = xISRwrap;
+  gptimer_register_event_callbacks(m_timer_handle, &cb_group, this);
+  m_alarm_config.flags.auto_reload_on_alarm = true;
 }
 
-/* Timer callback, used for generating pulses and calculating speed profile in real time */
-bool Stepper::xISR(gptimer_t *timer, const gptimer_alarm_event_data_t *data)
-{
-    GPIO.out_w1ts = (1ULL << m_step_pin);
+void Stepper::write_rad(float relative_rad) {
+    ESP_LOGI(TAG, "Setting relative angle");
+  if (relative_rad < 0) {
+    set_dir(!m_dir);
+  }
+  m_steps_to_go = (relative_rad / (2 * M_PI)) * m_steps_per_rotation;
+  m_step_count = 0;
+  m_alarm_config.alarm_count = m_step_interval;
+  
+  for(; m_steps_to_go > 0; m_steps_to_go--)
+  {
+      m_current_pos_steps += 1; 
+      gpio_set_level((gpio_num_t)m_step_pin, 1);
+      vTaskDelay(10/portTICK_PERIOD_MS);
+      gpio_set_level((gpio_num_t)m_step_pin, 0);
+      vTaskDelay(10/portTICK_PERIOD_MS);
+  }
+  //gptimer_set_alarm_action(m_timer_handle, &m_alarm_config);
+  //gptimer_enable(m_timer_handle);
+  //gptimer_start(m_timer_handle);
+}
 
+void Stepper::write_absolute_rad(float absolute_rad) {
 
-    // update current position
-    if (m_dir == CW)
-    {
-        m_current_pos_steps++;
-    }
-    else
-    {
-        m_current_pos_steps--;
-    }
+    ESP_LOGI(TAG, "Setting Abs angle");
+  // THIS IS NOT RIGHT BUT FUCK IT WE BALL
+  m_steps_to_go = (absolute_rad / 2 * M_PI) * m_steps_per_rotation;
+  m_step_count = 0;
+  m_alarm_config.alarm_count  = m_step_interval;
+  //gptimer_set_alarm_action(m_timer_handle, &m_alarm_config);
+  //gptimer_enable(m_timer_handle);
+  //gptimer_start(m_timer_handle);
+}
 
-    // we are done
-    if (m_steps_to_go == m_step_count)
-    {
-        gptimer_stop(m_timer_handle); // stop the timer
-        m_step_count = 0;
-        gptimer_disable(m_timer_handle);
-        GPIO.out_w1tc = (1ULL << m_step_pin);
-        return 0;
-    }
+void Stepper::disable_motor() { set_en(false); }
+void Stepper::enable_motor() { set_en(true); }
 
-    // TODO add acceleration
-
+void Stepper::set_speed(float speed_rad_per_sec, uint16_t acceleration_ms,
+                        uint16_t decceleration_ms) {
+  m_speed_rad_per_sec = speed_rad_per_sec;
     m_step_interval = TIMER_F / m_speed_rad_per_sec;
-
-    // set alarm to calculated interval and disable pin
-    GPIO.out_w1tc = (1ULL << m_step_pin);
-    m_alarm_cfg.alarm_count = m_step_interval;
-
-    gptimer_set_alarm_action(m_timer_handle, &alarm_cfg);
-    return 1;
+  // TODO
+  (void)acceleration_ms;
+  (void)decceleration_ms;
 }
 
+void Stepper::set_en(bool en_state) {
+  gpio_set_level((gpio_num_t)m_en_pin, en_state);
+}
+
+void Stepper::set_dir(bool dir_state) {
+  gpio_set_level((gpio_num_t)m_dir_pin, dir_state);
+}
+
+/* Timer callback, used for generating pulses and calculating speed profile in
+ * real time */
+bool Stepper::xISR(gptimer_t *timer, const gptimer_alarm_event_data_t *data) {
+  gpio_set_level((gpio_num_t)m_step_pin, 1);
+
+  // update current position
+  if (m_dir == CW) {
+    m_current_pos_steps++;
+  } else {
+    m_current_pos_steps--;
+  }
+
+  // we are done
+  if (m_steps_to_go == m_step_count) {
+    gptimer_stop(m_timer_handle); // stop the timer
+    m_step_count = 0;
+  gpio_set_level((gpio_num_t)m_step_pin, 0);
+    return 0;
+  }
+
+  // TODO add acceleration
+
+  
+
+  // set alarm to calculated interval and disable pin
+  gpio_set_level((gpio_num_t)m_step_pin, 0);
+
+  return 1;
+}
